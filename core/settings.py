a0aab7db8220f8ece7c60d378057a299dd1c0a82
Shuhib1924 <shuhib_s@live.de>
2024-07-05 13:31:13 +0200

    before sensational order

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n)sr&_b1xiyz3!-&f+*w+_p=gpmc3dzna$1!8&8vgkt-wpvxtd'
+SECRET_KEY = "django-insecure-n)sr&_b1xiyz3!-&f+*w+_p=gpmc3dzna$1!8&8vgkt-wpvxtd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,62 +32,61 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 'livereload',
-    'p1',
-    'd1',
-    'whitenoise.runserver_nostatic',
-     'crispy_forms',
+    "p1",
+    "d1",
+    "whitenoise.runserver_nostatic",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # 'livereload.middleware.LiveReloadScript',
 ]
 
 
-
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'p1.views.all_categories',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "p1.views.all_categories",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -97,16 +96,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -114,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = "de"
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
@@ -126,20 +125,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 # print("STATIC_URL", STATIC_URL)
-STATIC_ROOT = BASE_DIR/'static/'
+# STATIC_ROOT = BASE_DIR / "static/"
 # print("STATIC_ROOT", STATIC_ROOT)
-# STATICFILES_DIRS = [BASE_DIR/'core/static/']
+STATICFILES_DIRS = [BASE_DIR / "static/"]
 # print("STATICFILES_DIRS", STATICFILES_DIRS)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = "media/"
 # print("MEDIA_URL", MEDIA_URL)
-MEDIA_ROOT = BASE_DIR/'static/media/'
+MEDIA_ROOT = BASE_DIR / "static/media/"
 # print("MEDIA_ROOT", MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SESSION_COOKIE_AGE = 1800
--- p1/views.py
+++ p1/views.py
@@ -10,47 +10,53 @@ from .forms import OrderForm
 from escpos.printer import Network
 
 
-
 def home(request):
-    products = Product.objects.all().order_by('-created_date')[:4]
+    products = Product.objects.all().order_by("-created_date")[:4]
     categories = Category.objects.all()
-    context= {
-        'products':products,
-        'categories':categories,
+    context = {
+        "products": products,
+        "categories": categories,
     }
-    return render(request, 'p1/index.html', context)
+    return render(request, "p1/index.html", context)
+
 
 def all_categories(request):
     all_categories = Category.objects.all()
     return {"all_categories": all_categories}
+
 
 def category(request, cid):
     category = Category.objects.get(id=cid)
     products = Product.objects.filter(category=category)
     product_count = products.count()
     context = {
-        'category': category,
-        'products': products,
-        'product_count': product_count,
+        "category": category,
+        "products": products,
+        "product_count": product_count,
     }
-    return render(request,'p1/category.html', context)
+    return render(request, "p1/category.html", context)
+
 
 def detail(request, pid):
     if request.session.session_key:
-        print(Session.objects.get(pk=request.session.session_key).get_decoded(), request.session.session_key)
+        print(
+            Session.objects.get(pk=request.session.session_key).get_decoded(),
+            request.session.session_key,
+        )
     product = Product.objects.get(id=pid)
     context = {
-        'product':product,
+        "product": product,
     }
     # return HttpResponse(f"{product.name}")
-    return render(request, 'p1/detail.html', context)
+    return render(request, "p1/detail.html", context)
+
 
 def add_cart(request):
     # *get or create session
-    cart_session = request.session.get('cart')
+    cart_session = request.session.get("cart")
     if not cart_session:
-        cart_session=request.session.create()
-        cart_session = request.session['cart']=[]
+        cart_session = request.session.create()
+        cart_session = request.session["cart"] = []
 
     print("cart_session", cart_session)
     # %2. selected product from request
@@ -59,16 +65,16 @@ def add_cart(request):
         # $ check post request
         # print("post", request.POST, '\n')
         # * get product
-        get_product_id = request.POST.get('product')
+        get_product_id = request.POST.get("product")
         selected_product = get_object_or_404(Product, id=get_product_id)
-        print("selected_product", selected_product,'\n')
+        print("selected_product", selected_product, "\n")
         # cart_session.append(selected_product)
         # request.session['cart']= cart
 
-        get_variation_list = request.POST.getlist('variations[]')
+        get_variation_list = request.POST.getlist("variations[]")
         selected_variations = []
         for vid in get_variation_list:
-            variation= get_object_or_404(Variation, id=vid)
+            variation = get_object_or_404(Variation, id=vid)
             selected_variations.append(variation)
             print("variation", variation)
         print("selected_variations", selected_variations)
@@ -89,8 +95,8 @@ def add_cart(request):
         # variations.update(selected=True)
 
         #  add into cart and save it into session
-        cart_session.append({'cart_item': cart_item.id})
-        request.session['cart']= cart_session
+        cart_session.append({"cart_item": cart_item.id})
+        request.session["cart"] = cart_session
         print(">>", request.session)
         # request.session['cart'] == cart_session
         # print(request.POST.getlist('variations[]'))
@@ -99,138 +105,143 @@ def add_cart(request):
         #     variation = Variation.objects.filter(name=i)
         #     print('variation', variation)
 
-
     #     # print(request.POST)
-        # print([i for i in request.POST])
-        # session_product = request.POST.get('product')
-        # session_variation_list = request.POST.getlist('variations[]')
-        # # % 3. save data to session
-        # # request.session['selected_variations'] = session_variation_list
-        # # request.session['product'] = session_product
-        # # request.session.modified = True
-        # product = Product.objects.get(name=session_product)
-        # # print("product", product)
-        # variations = Variation.objects.filter(name__in=session_variation_list)
-        # # print("variations", variations)
+    # print([i for i in request.POST])
+    # session_product = request.POST.get('product')
+    # session_variation_list = request.POST.getlist('variations[]')
+    # # % 3. save data to session
+    # # request.session['selected_variations'] = session_variation_list
+    # # request.session['product'] = session_product
+    # # request.session.modified = True
+    # product = Product.objects.get(name=session_product)
+    # # print("product", product)
+    # variations = Variation.objects.filter(name__in=session_variation_list)
+    # # print("variations", variations)
 
     # return redirect('cart')
-    return JsonResponse({'message': 'success'})
+    return JsonResponse({"message": "success"})
+
 
 def cart(request):
-    data = request.session.get('cart', [])
+    data = request.session.get("cart", [])
     print("data", data)
     items = []
     if data:
         for el in data:
-            obj = get_object_or_404(CartItem, id=el['cart_item'])
+            obj = get_object_or_404(CartItem, id=el["cart_item"])
             print("obj", obj)
             items.append(obj)
-    print("ITEMS",items)
+    print("ITEMS", items)
     cart_sum = 0
     for ci in items:
         cart_sum = cart_sum + ci.pro_var_sum()
     # print("cart_sum", cart_sum)
-    netto = cart_sum *81 /100
-    tax = cart_sum *19 /100
+    netto = cart_sum * 81 / 100
+    tax = cart_sum * 19 / 100
 
     # if request.session.session_key:
     #         print(Session.objects.get(pk=request.session.session_key).get_decoded(), request.session.session_key)
     context = {
-        'cart_sum': cart_sum,
-        'netto': netto,
-        'tax': tax,
-        'items':items,
-        }
-    return render(request, 'p1/cart.html', context)
+        "cart_sum": cart_sum,
+        "netto": netto,
+        "tax": tax,
+        "items": items,
+    }
+    return render(request, "p1/cart.html", context)
+
 
 def delete_item(request, id):
     item = get_object_or_404(CartItem, id=id)
-    sk = request.session.session_key
-    objects = Session.objects.get(pk=request.session.session_key).get_decoded()
-    cart = request.session.get('cart', [])
-    print("cart", cart, '\n')
+    cart = request.session.get("cart", [])
+    print("cart", cart, "\n")
     # delete from session
     for i in cart:
-        if i['cart_item']==id:
+        if i["cart_item"] == id:
             cart.remove(i)
     print(cart)
     # save it into session
-    request.session['cart']=cart
+    request.session["cart"] = cart
     # delete from db
     item.delete()
     # return HttpResponse(f'CartItem ID: {item.id}<hr>Product: {item.product.name} <hr> items count: {len(cart)}<hr> key: {sk} <hr> all objects: {objects}<hr>')
-    return redirect('cart')
+    return redirect("cart")
 
 
 def checkout(request):
     form = OrderForm()
-    data= request.session.get('cart', [])
+    data = request.session.get("cart", [])
     print("data", data)
     items = []
     if data:
         for el in data:
-            obj = get_object_or_404(CartItem, id=el['cart_item'])
+            obj = get_object_or_404(CartItem, id=el["cart_item"])
             print("obj", obj)
             items.append(obj)
-    print("ITEMS",items)
+    print("ITEMS", items)
     cart_sum = 0
     for ci in items:
         cart_sum = cart_sum + ci.pro_var_sum()
     print("cart_sum", cart_sum)
     if request.session.session_key:
-        print(Session.objects.get(pk=request.session.session_key).get_decoded(), request.session.session_key)
-    return render(request, 'p1/checkout.html', {'form': form})
+        print(
+            Session.objects.get(pk=request.session.session_key).get_decoded(),
+            request.session.session_key,
+        )
+    return render(request, "p1/checkout.html", {"form": form})
+
 
 def printing(request):
     if request.session.session_key:
-        print(Session.objects.get(pk=request.session.session_key).get_decoded(), request.session.session_key)
-    data = request.session.get('cart', [])
+        print(
+            Session.objects.get(pk=request.session.session_key).get_decoded(),
+            request.session.session_key,
+        )
+    data = request.session.get("cart", [])
     print("data", data)
     items = []
     if data:
         for el in data:
-            obj = get_object_or_404(CartItem, id=el['cart_item'])
+            obj = get_object_or_404(CartItem, id=el["cart_item"])
             print("obj", obj)
             items.append(obj)
-    print("ITEMS",items)
+    print("ITEMS", items)
     cart_sum = 0
     for ci in items:
         cart_sum = cart_sum + ci.pro_var_sum()
     # print("cart_sum", cart_sum)
-    netto = cart_sum *81 /100
-    tax = cart_sum *19 /100
+    netto = cart_sum * 81 / 100
+    tax = cart_sum * 19 / 100
 
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.POST)
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        pickup_time = request.POST.get('pickup_time')
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        pickup_time = request.POST.get("pickup_time")
         order = Order.objects.create(name=name, phone=phone, pickup_time=pickup_time)
         # order.total = request.session
         # Set the IP address of the printer
-        printer_ip = '192.168.178.177'
+        printer_ip = "192.168.178.177"
         # Initialize the network connection to the printer
         epson = Network(printer_ip)
 
         # Send data to the printer
-        epson.text(f'{order.pickup_time}\n')
-        epson.text(f'{order.id}\n')
+        epson.text(f"{order.pickup_time}\n")
+        epson.text(f"{order.id}\n")
         for el in data:
-            obj = get_object_or_404(CartItem, id=el['cart_item'])
+            obj = get_object_or_404(CartItem, id=el["cart_item"])
             print("obj", obj)
-            epson.text(f'{obj.product}\n')
+            epson.text(f"{obj.product}\n")
             for vl in obj.variations.all():
-                epson.text(f'{vl}\n')
+                epson.text(f"{vl}\n")
 
         epson.cut()
-
 
         # if order.daily_id.last() == None:
         #     order.daily_id =1
         # else:
         #     order.daily_id = order.daily_id + 1
         # order.save()
-        context = {'order': order}
+        context = {"order": order}
 
         # form = OrderForm(request.POST)
         # if form.is_valid():
@@ -251,11 +262,13 @@ def printing(request):
         # ! ganz am ende!!!!
         # request.session.flush()
     if request.method == "GET":
-        return redirect('failed')
+        return redirect("failed")
     return render(request, "p1/payment_success.html", context)
 
+
 def payment_success(request):
-    return HttpResponse('success')
+    return HttpResponse("success")
+
 
 def failed(request):
-    return render(request, 'p1/failed.html')
+    return render(request, "p1/failed.html")
